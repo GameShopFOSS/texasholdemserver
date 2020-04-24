aa55e37f030012afd9e9c4600bf449e9c95ffbd9
jayevans1 <jayevans@live.com>
2020-04-24 05:07:56 -0400

    decrypt aes-192-cbc

--- encryption-service.js
+++ encryption-service.js
@@ -1,7 +1,8 @@
 
 var crypto = require('crypto'),
-  algorithm = 'aes-256-gcm',
-  password = 'ABCDEFGHJKLMNOPQRSTUVWXYZABCDEFG',
+  //algorithm = 'aes-256-gcm',
+  algorithm = 'aes-192-cbc',
+  password = 'ABCDEFGHJKLMNOPQRSTUVWXY',//ZABCDEFG',
   // do not use a global iv for production, 
   // generate a new one for each encryption
   iv = 'ABCDEFGHIJKLMNOP'
@@ -25,11 +26,11 @@ constructor() {
  decrypt(encrypted) {
   var decipher = crypto.createDecipheriv(algorithm, password, iv);
   //decipher.setAuthTag(encrypted.tag);
-    var cipher = crypto.createCipheriv(algorithm, password, iv);
-var encryptedC = cipher.update(text, 'utf8', 'hex');
-  encryptedC += cipher.final('hex');
-  var tag = cipher.getAuthTag();
-  decipher.setAuthTag(tag);
+//     var cipher = crypto.createCipheriv(algorithm, password, iv);
+// var encryptedC = cipher.update(, 'utf8', 'hex');
+//   encryptedC += cipher.final('hex');
+  //var tag = cipher.getAuthTag();
+  //decipher.setAuthTag(tag);
 
   var dec = decipher.update(encrypted, 'hex', 'utf8');//.encrypted.content
   dec += decipher.final('utf8');
