1b8dab04c471f1909fac4c82917a58ca4b2e033e
jayevans1 <jayevans@live.com>
2020-04-24 03:46:59 -0400

    new constructor global2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {MongoClient} = require('mongodb');
 var bodyParser = require('body-parser');
 
 //const http = require('http');
-const {EncryptionService} = require('./encryption-service');
+const EncryptionService = require('./encryption-service').EncryptionService;
 var encryptionService = new EncryptionService();
 const hostname = '127.0.0.1';
 const port = 8080;
