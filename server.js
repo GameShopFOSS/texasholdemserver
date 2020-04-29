96fe813ec77fe8df6bcd900e41ce276b64b27627
jayevans1 <jayevans@live.com>
2020-04-29 17:59:52 -0400

    pre-beta-build-2

--- server.js
+++ server.js
@@ -602,14 +602,14 @@ async function transitionScene(requestBody){
    await collection.updateOne(myquery, newvalues, function(err, res) {
     if (err) throw err;
     console.log(requestBody.email + " changed scene to " +  requestBody.destination);
-  
+  return "" + requestBody.destination;
     //db.close();
   });
    //return data.game
      //result = {chips: data.chips, gameScene: data.gameScene};
   // });
 
-
+return "Failed";
 // console.log(emailAddress);
 //    console.log(emailAddress.length);
 // 	 if (emailAddress.length > 0){
